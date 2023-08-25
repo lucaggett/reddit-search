298355a790952a340f083b45b0170ef38e4430cc
Luc <luc@aggett.com>
2023-08-25 10:08:47 +0200

    update main to newest version

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ use zstd::Decoder;
 use std::io::{BufRead, BufReader, Write, BufWriter};
 use clap::{command, Parser};
 use std::collections::HashMap;
-use std::thread::sleep;
 use indicatif::{ProgressBar, ProgressStyle};
 use rayon::prelude::*;
+use std::fs::OpenOptions;
 
+const CHUNK_SIZE: usize = 100000;
 #[derive(Parser, Debug)]
 #[command(name = "reddit-search")]
 #[command(author = "Luc Aggett (luc@aggett.com)")]
@@ -17,14 +18,16 @@ struct Args {
     #[arg(short, long)]
     input: PathBuf,
     #[arg(short, long, default_value = "output.json")]
-    output: PathBuf,
-    #[arg(num_args(0..))]
+    output: String,
+    #[arg(short, long, default_value = "20000")]
+    chunk_size: usize,
+    #[arg(num_args(1..), short, long)]
     fields: Vec<String>,
     #[arg(short, long, default_value = "false")]
     verbose: bool,
+    #[arg(short, long, default_value = "false")]
+    append: bool,
 }
-
-const CHUNK_SIZE: usize = 10_000; // Adjust based on experimentation
 
 fn process_line(line: String, field_map: &HashMap<String, String>) -> Option<String> {
     if field_map.iter().all(|(field, value)| {
@@ -45,42 +48,49 @@ fn process_chunk(lines: Vec<String>, field_map: &HashMap<String, String>) -> Vec
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    // copy the input path so we can use it in the progress bar
-    let input_path = args.input.clone().into_os_string().into_string().unwrap();
+    // copy the input path so we can use it for a message later
+    let input_path = args.input.clone();
     let input_file = File::open(args.input)?;
+    let metadata = input_file.metadata()?;
 
     let mut decoder = Decoder::new(input_file)?;
     decoder.window_log_max(31)?;
     let input_stream = BufReader::new(decoder);
 
-    let output_file = File::create(args.output)?;
+    let output_path = PathBuf::from(&args.output);
+    let output_file = OpenOptions::new()
+        .create(true)
+        .append(args.append)
+        .write(true)
+        .open(output_path.clone())?;
     let mut output_stream = BufWriter::new(output_file);
-
-
-    let pb = ProgressBar::new_spinner();
-    pb.set_style(ProgressStyle::default_spinner()
-        .template("[{elapsed}] Processing {spinner} {pos} lines").expect("Failed to set progress bar style")
-        .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ "));
-
-
-    let field_map: HashMap<String, String> = args.fields.iter().filter_map(|field| {
-        let parts: Vec<&str> = field.split(':').collect();
-        if parts.len() == 2 {
-            Some((parts[0].to_string(), parts[1].to_string()))
-        } else {
-            None
+    //println!("{:?}", args.fields);
+    let mut field_map: HashMap<String, String> = HashMap::new();
+    for field in args.fields {
+        let mut split = field.split(":");
+        let field = split.next().unwrap().to_string();
+        let value = split.next().unwrap().to_string();
+        if field_map.contains_key(&field) {
+            println!("{} already exists in field map, adding {} to existing value", field, value);
+            let existing_value = field_map.get(&field.clone()).unwrap().to_string();
+            field_map.insert(field.clone(), format!("{}|{}", existing_value, value));
         }
-    }).collect();
-
-    if field_map.len() == 1 {
-        println!("Fetching all comments with {} = {} from {}", field_map.keys().next().unwrap(), field_map.values().next().unwrap(), input_path);
-    } else {
-        println!("Fetching all comments with the following fields from {}", input_path);
-        for (field, value) in field_map.iter() {
-            println!("{} = {}", field, value);
+        else {
+            field_map.insert(field, value);
         }
     }
-    sleep(std::time::Duration::from_millis(2000));
+    //println!("{:?} {:?}", field_map, field_map.len());
+    // if field_map.len() == 1 {
+    //     println!("Fetching all comments with {} = {} from {}", field_map.keys().next().unwrap(), field_map.values().next().unwrap(), input_path);
+    // } else {
+    //     println!("Fetching all comments with the following fields from {}", input_path);
+    //     for (field, value) in field_map.iter() {
+    //         println!("{} = {}", field, value);
+    //     }
+    // }
+
+    println!("Starting reddit-search for {} ({} threads)", input_path.display(), rayon::current_num_threads());
+
     let (tx, rx) = std::sync::mpsc::channel();
 
     rayon::spawn(move || {
@@ -103,6 +113,12 @@ fn main() -> std::io::Result<()> {
     let mut matched_lines_count = 0;
     let mut total_lines = 0;
 
+    // estimate the number of lines by multiplying the number of GB by 8000000 (This is an estimate I got from looking at a few sample files)
+    let estimated_num_lines = ((metadata.len() as f64 / 1_000_000_000.0) * 10_000_000.0) as u64;
+    let pb = ProgressBar::new_spinner();
+    pb.set_style(ProgressStyle::default_spinner()
+        .template("[{elapsed_precise}] {pos} lines processed (~{msg}%)").expect("Failed to set progress bar style")
+        .tick_chars("-/||\\-"));
     for chunk in rx {
         let matches = process_chunk(chunk, &field_map);
         matched_lines_count += matches.len();
@@ -111,13 +127,18 @@ fn main() -> std::io::Result<()> {
         for line in matches {
             writeln!(output_stream, "{}", line)?;
         }
-        if total_lines % 250_000 == 0 {
+        if total_lines % 100_000 == 0 {
             pb.set_position(total_lines as u64); // Update progress bar with lines processed
+            pb.set_message(format!("{:.2}", (total_lines as f64 / estimated_num_lines as f64) * 100.0));
         }
     }
 
-    pb.finish_with_message("done");
+    pb.finish_with_message("100");
     println!("Matched {} lines out of {}", matched_lines_count, total_lines);
+    if matched_lines_count == 0 {
+        println!("No matches found, deleting output file");
+        std::fs::remove_file(output_path)?;
+    }
 
     Ok(())
 }
